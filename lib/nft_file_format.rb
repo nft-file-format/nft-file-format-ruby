7ee81f15f608fd01f374d76f4eb9074541c0cfa4
sebi <gore.sebyx@yahoo.com>
2021-12-26 09:18:02 +0000

    init

---
+++ lib/nft_file_format.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nft_file_format/version"
+
+module NftFileFormat
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nft_file_format/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NftFileFormat
+  VERSION = "0.1.0"
+end
